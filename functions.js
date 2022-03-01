94d1b18738fa8c65a09a325f4871c4e5dc750e8a
Jeremy Lincoln <jeremy.lincoln@bisonline.com>
2022-03-01 17:00:04 -0500

    first commit 2

--- functions.js
+++ functions.js
@@ -1,7 +1,22 @@
 // Arrow functions (new with es6)
 
+//when using {}, you need a return statement
 const add = (n1, n2) => {
   return n1 + n2;
 }
 
+// if we don't {}, then its implicitly returned
+const add1 = (n1, n2) => n1 + n2
+
+
+//es5 declaring
+
+function add3(n1,n2) {
+  return n1 + n2
+}
+
 console.log(add(2,3))
+console.log(add1(2,3))
+console.log(add3(2,3))
+
+https://youtu.be/_-ZCXjJX3kc?t=590
